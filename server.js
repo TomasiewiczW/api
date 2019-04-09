5400378a12fa0ceff0e3c30845137600e5ea6ef0
TomasiewiczW <wojtarek@vp.pl>
2019-04-09 15:58:43 +0200

    First
    Small changes to database, added many new functions to server, so it can respond and read useful data from worker's database

--- server.js
+++ server.js
@@ -5,24 +5,50 @@ const port = 3000;
 const sqlite3 = require('sqlite3').verbose();
 let db =  new sqlite3.Database('chinook.db');
 
-app.get('/get', (req, res) => {
-    db.all('SELECT Title FROM albums Where AlbumId = 4', (err, rows) => {
-        console.log(rows);
-        res.send(rows);
+app.post('/AddWorker', (req, res) => {
+    db.all(`INSERT INTO PracownicyZatrudnieni VALUES (${req.query.PersonId}, ${req.query.LastName}, ${req.query.FirstName}, ${req.query.NFCCode})`, (err, rows) => {
+        if(err){
+            return console.log(err);
+        }
+        return res.send(rows);
     })
 });
-    // db.serialize(() => {
-    //     let arr = [];
-    //     db.each('SELECT * FROM albums', (err, row) => {
-    //       if (err) {
-    //         console.error(err.message);
-    //       }
-    //       arr.push(row);
-    //     });
-    //       res.send(arr)
-    //     }); 
-    //   });
 
+app.post('/WorkerScanned', (req, res) => {
+    db.all(`INSERT INTO Wejscia VALUES (${req.query.PersonId}, ${req.query.LastName}, ${req.query.FirstName}, ${req.query.DateAndTime})`, (err, rows) => {
+        if(err){
+            return console.log(err);
+        }
+        return res.send(rows);
+    })
+});
+
+app.get('/FindWorkerByNfcCode', (req, res) =>{
+    db.all(`SELECT * FROM PracownicyZatrudnieni WHERE NFCCode = ${req.query.NfcCode}`, (err, rows) => {
+        if(err) return console.log(err);
+        return res.send(rows);
+    })
+})
+
+app.get('/GetAllFromTable', (req, res) => {
+    db.all(`SELECT * FROM ${req.query.Table}`, (err, rows) => {
+        if(err) return console.log(err);
+        return res.send(rows);
+    })    
+});
+
+app.post('/CreateDebug', (req, res) => {
+    db.all(`CREATE TABLE Wejscia (PersonID int, LastName varchart(255), FirstName varchart(255), DateAndTime datetime)`, (err, rows) => {
+        if(err) return console.log(err);
+    })    
+});
+
+app.get('/GetAllTables', (req, res) => {
+    db.all(`SELECT name FROM sqlite_master WHERE type IN ('table','view') AND name NOT LIKE 'sqlite_%' ORDER BY 1`, (err, rows) => {
+        if(err) return console.log(err);
+        res.send(rows);
+    })    
+});
 
 app.use(bodyParser.json());
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
