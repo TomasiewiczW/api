e4a6d34abeb75438cfa92931a36ebd2f7109753f
TomasiewiczW <wojtarek@vp.pl>
2019-04-23 14:29:46 +0200

    Third
    Added relations in database, added some functions that helps communicate with database to server file, Iscurrentrlyworking acts as flipflop

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const app = express();
 const port = 3000;
 const sqlite3 = require('sqlite3').verbose();
 let db =  new sqlite3.Database('chinook2.db');
-
-let working = false;
+let wejscia = 0;
+let working = 0;
 
 
 app.post('/AddWorker', (req, res) => {
-    db.all(`INSERT INTO PracownicyZatrudnieni VALUES (${req.query.PersonId}, ${req.query.LastName}, ${req.query.FirstName}, ${req.query.NFCCode})`, (err, rows) => {
+    db.all(`INSERT INTO PracownicyZatrudnieni VALUES (${req.query.PersonId}, ${req.query.LastName}, ${req.query.FirstName}, ${req.query.NFCCode}, 0)`, (err, rows) => {
         if(err){
             return console.log(err);
         }
@@ -18,14 +18,7 @@ app.post('/AddWorker', (req, res) => {
 });
 
 app.post('/WorkerScanned', (req, res) => {
-    db.all(`INSERT INTO Wejscia VALUES (${req.query.PersonId}, ${req.query.LastName}, ${req.query.FirstName}, ${req.query.DateAndTime})`, (err, rows) => {
-        
-        if(working){
-            db.all(`UPDATE PracownicyZatrudnieni SET isCurrentlyWorking = 0 WHERE (FirstName = \"${req.query.FirstName}\")`)
-        }
-        else{
-            db.all(`UPDATE PracownicyZatrudnieni SET isCurrentlyWorking = 1 WHERE (FirstName = \"${req.query.FirstName}\")`)
-        }
+    db.all(`INSERT INTO Wejscia (FirstName, LastName, DateAndTime) VALUES (${req.query.FirstName}, ${req.query.LastName}, ${req.query.DateAndTime})`, (err, rows) => {
         if(err){
             return console.log(err);
         }
@@ -34,14 +27,22 @@ app.post('/WorkerScanned', (req, res) => {
 });
 
 app.get('/GetWorkerStatus', (req, res) => {
-    db.all(`SELECT isCurrentlyWorking FROM PracownicyZatrudnieni WHERE (FirstName = "${req.query.firstname}" AND LastName = "${req.query.lastname}")`, (err, rows) => {
+    db.all(`SELECT isCurrentlyWorking FROM PracownicyZatrudnieni WHERE (FirstName = ${req.query.FirstName} AND LastName = ${req.query.LastName})`, (err, rows) => {
         if(err){
             return console.log(err);
         }
-        working = rows[0].isCurrentlyWorking;
-        console.log(working);
-        return res.send(rows);
-    })    
+        wejscia = rows[0].IsCurrentlyWorking;
+        console.log(wejscia);
+        return wejscia
+    })
+});
+
+app.post('/SetWorkerStatus', (req, res) => {
+    db.all(`UPDATE PracownicyZatrudnieni SET isCurrentlyWorking = ${req.query.WorkerStatus} WHERE (FirstName = ${req.query.FirstName} AND LastName = ${req.query.LastName})`, (err, rows) => {
+        if(err){
+            return console.log(err);
+        }
+    })
 });
 
 app.get('/FindWorkerByNfcCode', (req, res) => {
@@ -61,7 +62,7 @@ app.get('/GetAllFromTable', (req, res) => {
 app.post('/CreateDebug', (req, res) => {
     db.all(`CREATE TABLE Wejscia (PersonID int, LastName varchart(255), FirstName varchart(255), DateAndTime datetime)`, (err, rows) => {
         if(err) return console.log(err);
-    })    
+    })
 });
 
 app.get('/GetAllTables', (req, res) => {
